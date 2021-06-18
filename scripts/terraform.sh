4042396fc1d681b0e42d65a331138e69c4e564e2
Nathalia <mdc.nathalia@gmail.com>
2021-06-18 15:35:07 +0300

    Bumped terraform version

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -4,20 +4,20 @@
 #          Installing Terraform             #
 #############################################
 
-echo '[+] Downloading Terraform v0.14.10...'
-curl -LO https://releases.hashicorp.com/terraform/0.14.10/terraform_0.14.10_linux_amd64.zip
+echo '[+] Downloading Terraform v1.0.0...'
+curl -LO https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_linux_amd64.zip
 
 # Creates temporary directory if does not exist
 sudo mkdir -p terraform
 
 # Extracts file to temp directory
-sudo unzip terraform_0.14.10_linux_amd64.zip -d terraform
+sudo unzip terraform_1.0.0_linux_amd64.zip -d terraform
 
 # Install it
-echo '[+] Installing Terraform v0.14.10...'
+echo '[+] Installing Terraform v1.0.0...'
 sudo install -o root -g root -m 0755 terraform/terraform /usr/local/bin/terraform
 
 # Cleanup files
 echo '[+] Cleaning up files...'
-sudo rm -r terraform terraform_0.14.10_linux_amd64.zip
+sudo rm -r terraform terraform_1.0.0_linux_amd64.zip
 
