d2028126518ebb60d115bbeaff3fecd73ecc3ac7
Nathalia <mdc.nathalia@gmail.com>
2021-09-17 16:48:25 +0300

    Added istio

---
+++ scripts/istio.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+echo "[+] Downloading Istio..."
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.11.2 sh -
+
+echo "[+] Installing Istio..."
+sudo mv istio-1.11.2/bin/istioctl /usr/local/bin/istioctl
+
+echo "[+] Cleaning up files..."
+sudo rm -r istio-1.11.2
