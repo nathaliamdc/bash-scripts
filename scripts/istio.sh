311552d684c423a4757f90b4b069c5f04086410f
Nathalia <mdc.nathalia@gmail.com>
2021-11-05 23:42:19 +0200

    Bumped istio version to 1.11.4

--- scripts/istio.sh
+++ scripts/istio.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
 echo "[+] Downloading Istio..."
-curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.11.2 sh -
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.11.4 sh -
 
 echo "[+] Installing Istio..."
-sudo mv istio-1.11.2/bin/istioctl /usr/local/bin/istioctl
+sudo mv istio-1.11.4/bin/istioctl /usr/local/bin/istioctl
 
 echo "[+] Cleaning up files..."
-sudo rm -r istio-1.11.2
+sudo rm -r istio-1.11.4
